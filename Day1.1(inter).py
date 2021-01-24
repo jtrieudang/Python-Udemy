f0df7576a08560cd4107c58f67d7a1490d55aa9a
Jimmy T Trieudang <62171921+jtrieudang@users.noreply.github.com>
2021-01-24 15:54:49 -0800

    Add files via upload

---
+++ Day1.1(inter).py
@@ -0,0 +1,33 @@
+# name = "Jack"
+# print(name)
+# # Changes the variable
+# name = "jimmy"
+# print(name)
+
+# name = input("What is your name?")
+# length = len(name)
+# print(length)
+
+# practice variable and length
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = a 
+a = b
+b = c
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+user_name = input("What is your name?")
+length = len(user_name)
+print(length)
