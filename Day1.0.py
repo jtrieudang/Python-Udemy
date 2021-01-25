31a3155dd049135ffb84e0d02d059704569c39b3
Jimmy T Trieudang <62171921+jtrieudang@users.noreply.github.com>
2021-01-24 16:08:09 -0800

    Add files via upload

---
+++ Day1.0.py
@@ -0,0 +1,36 @@
+# Write your code below this line 👇
+print("hello world!")
+print('Day 1 - Python Print Function')
+print('The function is declared like this:')
+print("print('what to print')")
+# Return to the front
+print("Hello world!\nHello World!\n Hello Dude!")
+# concatenate, taking seperate strings
+print("Hello" + "Jimmy")
+print("Hello" + " Jimmy")
+# space sensitive, don't space in the front of the code
+print("Hello" + " " + "Jimmy")
+
+# __debug__
+#Fix the code below 👇
+
+# #Fix the code below 👇
+
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+  # print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "'+'" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# input function to put in data and return back.
+input("What is your name?")
+print("Hello " + input("What is your name?"))
+
+print(len(input("What is your name?")))
+# assigning to a variable
+name = "Jimmy"
+print(name)
