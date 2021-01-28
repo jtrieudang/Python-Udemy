6b62c49b38d4361918f7fb999ace8be74c3a16af
Jimmy T Trieudang <62171921+jtrieudang@users.noreply.github.com>
2021-01-28 12:58:10 -0800

    Add files via upload

---
+++ Day2.1(inter).py
@@ -0,0 +1,24 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+# my code
+# print(float(two_digit_number[0])+
+# float(two_digit_number[1]))
+
+
+# solution:
+# first_digit = two_digit_number[0]
+# second_digit = two_digit_number[1]
+
+# result = int(first_digit) + int(second_digit)
+
+# print(result)
+
+# practice
+first_num = two_digit_number[0]
+second_num = two_digit_number[1]
+sol = int(first_num) + int(second_num)
+print(sol)
