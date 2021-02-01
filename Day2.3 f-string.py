e07d2bec491d5b1147d6857eca769402adf0c6b6
Jimmy T Trieudang <62171921+jtrieudang@users.noreply.github.com>
2021-02-01 13:42:28 -0800

    Add files via upload

---
+++ Day2.3 f-string.py
@@ -0,0 +1,16 @@
+print(round(8/3, 2))
+
+# two // becomes int
+# single / becomes a float
+
+# +=
+# -=
+# /=
+# *=
+# all by 1
+
+score = 0
+height = 1.8
+isWinning = True
+# instead of using string and +, we will use f-string
+print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
